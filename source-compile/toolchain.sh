f18107cd99e50dea5f0d718d494b5176646a065e
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2020-04-06 12:12:34 +0300

    Added PE targets to binutils to allow creating UEFI executables.

--- source-compile/toolchain.sh
+++ source-compile/toolchain.sh
@@ -4,6 +4,18 @@ set -e
 target=$1
 prefix=/usr/cross/$target
 
+case $target in
+i686-pc-elf)
+	targets=$target,i686-pc-pe
+	;;
+x86_64-pc-elf)
+	targets=$target,x86_64-pc-pe
+	;;
+*)
+	targets=$target
+	;;
+esac
+
 gcc=`ls -1 /usr/src | grep -o 'gcc-[0-9]\+.[0-9]\+.[0-9]\+'`
 binutils=`ls -1 /usr/src | grep -o 'binutils-[0-9]\+.[0-9]\+\(.[0-9]\+\)\?'`
 newlib=`ls -1 /usr/src | grep -o 'newlib-[0-9]\+.[0-9]\+.[0-9]\+'`
@@ -17,7 +29,7 @@ mkdir -p /tmp/toolchain/build-gcc
 mkdir -p /tmp/toolchain/build-newlib
 
 cd /tmp/toolchain/build-binutils
-/usr/src/$binutils/configure --target=$target --prefix=$prefix --disable-nls 2>&1
+/usr/src/$binutils/configure --target=$target --enable-targets=$targets --prefix=$prefix --disable-nls 2>&1
 make -j $(nproc --all) all 2>&1
 make install 2>&1
 
