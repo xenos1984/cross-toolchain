81c74c855a844e55ce85d6733c0843e78d080153
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2020-04-06 12:11:55 +0300

    Added patch to build x86_64 gcc with red zone disabled.

--- source-compile/download.sh
+++ source-compile/download.sh
@@ -15,6 +15,22 @@ wget -c -O /tmp/gcc-$gcc_version.tar.xz ftp://ftp.gnu.org/gnu/gcc/gcc-$gcc_versi
 tar -xf /tmp/gcc-$gcc_version.tar.xz
 rm /tmp/gcc-$gcc_version.tar.xz
 
+cat << EOF > gcc-$gcc_version/gcc/config/i386/t-x86_64-elf
+# Add libgcc multilib variant without red-zone requirement
+
+MULTILIB_OPTIONS += mno-red-zone
+MULTILIB_DIRNAMES += no-red-zone
+EOF
+
+patch -u --ignore-whitespace gcc-$gcc_version/gcc/config.gcc << 'EOF'
+--- config.gcc
++++ /tmp/config.gcc
+@@ -1638,2 +1638,3 @@
+ x86_64-*-elf*)
++	tmake_file="${tmake_file} i386/t-x86_64-elf" # include the new multilib configuration
+ 	tm_file="${tm_file} i386/unix.h i386/att.h dbxelf.h elfos.h newlib-stdint.h i386/i386elf.h i386/x86-64.h"
+EOF
+
 wget -c -O /tmp/binutils-$binutils_version.tar.bz2 ftp://ftp.gnu.org/gnu/binutils/binutils-$binutils_version.tar.bz2
 tar -xf /tmp/binutils-$binutils_version.tar.bz2
 rm /tmp/binutils-$binutils_version.tar.bz2
