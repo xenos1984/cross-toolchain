b6e51fd71841a28224df24900e63f064f54ee2bd
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2020-04-29 21:22:45 +0300

    Try patch against NUMA error in docker.

--- test-qemu/build.sh
+++ test-qemu/build.sh
@@ -6,7 +6,7 @@ qemu=`ls -1 /usr/src | grep -o 'qemu-[0-9]\+.[0-9]\+.[0-9]\+'`
 mkdir -p /tmp/build-qemu
 cd /tmp/build-qemu
 /usr/src/$qemu/configure --disable-user
-make
+make -j $(nproc --all)
 make install
 cd /tmp
 rm -rf /tmp/build-qemu /usr/src/$qemu
--- tools-qemu/download.sh
+++ tools-qemu/download.sh
@@ -9,3 +9,13 @@ cd /usr/src
 wget -c -O /tmp/qemu-$version.tar.xz http://download.qemu.org/qemu-$version.tar.xz
 tar -xf /tmp/qemu-$version.tar.xz
 rm /tmp/qemu-$version.tar.xz
+
+patch -u --ignore-whitespace backends/hostmem.c << 'EOF'
+--- a/backends/hostmem.c
++++ b/backends/hostmem.c
+@@ -384,3 +385,3 @@
+         if (mbind(ptr, sz, backend->policy,
+-                  maxnode ? backend->host_nodes : NULL, maxnode + 1, flags)) {
++                  maxnode ? backend->host_nodes : NULL, 0, flags)) {
+             if (backend->policy != MPOL_DEFAULT || errno != ENOSYS) {
+EOF
