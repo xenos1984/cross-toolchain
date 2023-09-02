82a275db48ad70866701495820d14c74cab69118
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2023-09-02 20:01:28 +0300

    Added Bochs Docker image.

---
+++ test-bochs/build.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+set -e
+
+cd /usr/src
+svn co http://svn.code.sf.net/p/bochs/code/trunk/bochs bochs
+cd bochs
+
+./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-smp --enable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
+make -j 8 all
+sudo make install
+make clean
+sudo mv `which bochs` `which bochs`64mpdbg
+
+./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-smp --enable-memtype
+make -j 8 all
+sudo make install
+make clean
+sudo mv `which bochs` `which bochs`64mp
+
+./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
+make -j 8 all
+sudo make install
+make clean
+sudo mv `which bochs` `which bochs`64dbg
+
+./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-memtype
+make -j 8 all
+sudo make install
+make clean
+sudo mv `which bochs` `which bochs`64
+
+./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-cpu-level=6 --enable-smp --enable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
+make -j 8 all
+sudo make install
+make clean
+sudo mv `which bochs` `which bochs`686mpdbg
+
+./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-cpu-level=6 --enable-smp --enable-memtype
+make -j 8 all
+sudo make install
+make clean
+sudo mv `which bochs` `which bochs`686mp
+
+./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-cpu-level=6 --enable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
+make -j 8 all
+sudo make install
+make clean
+sudo mv `which bochs` `which bochs`686dbg
+
+./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-cpu-level=6 --enable-memtype
+make -j 8 all
+sudo make install
+make clean
+sudo mv `which bochs` `which bochs`686
+
+./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --disable-usb --disable-pci --enable-cpu-level=4 --disable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
+make -j 8 all
+sudo make install
+make clean
+sudo mv `which bochs` `which bochs`486dbg
+
+./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --disable-usb --disable-pci --enable-cpu-level=4 --disable-memtype
+make -j 8 all
+sudo make install
+make clean
+sudo mv `which bochs` `which bochs`486
+
+cd ..
+rm -rf bochs
