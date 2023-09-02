3f922093aa2a5a3c95f889ce9cc0502874e0a73d
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2023-09-02 20:22:31 +0300

    No need to sudo in Bochs build script.

--- test-bochs/build.sh
+++ test-bochs/build.sh
@@ -7,63 +7,63 @@ cd bochs
 
 ./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-smp --enable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
 make -j 8 all
-sudo make install
+make install
 make clean
-sudo mv `which bochs` `which bochs`64mpdbg
+mv `which bochs` `which bochs`64mpdbg
 
 ./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-smp --enable-memtype
 make -j 8 all
-sudo make install
+make install
 make clean
-sudo mv `which bochs` `which bochs`64mp
+mv `which bochs` `which bochs`64mp
 
 ./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
 make -j 8 all
-sudo make install
+make install
 make clean
-sudo mv `which bochs` `which bochs`64dbg
+mv `which bochs` `which bochs`64dbg
 
 ./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-memtype
 make -j 8 all
-sudo make install
+make install
 make clean
-sudo mv `which bochs` `which bochs`64
+mv `which bochs` `which bochs`64
 
 ./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-cpu-level=6 --enable-smp --enable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
 make -j 8 all
-sudo make install
+make install
 make clean
-sudo mv `which bochs` `which bochs`686mpdbg
+mv `which bochs` `which bochs`686mpdbg
 
 ./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-cpu-level=6 --enable-smp --enable-memtype
 make -j 8 all
-sudo make install
+make install
 make clean
-sudo mv `which bochs` `which bochs`686mp
+mv `which bochs` `which bochs`686mp
 
 ./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-cpu-level=6 --enable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
 make -j 8 all
-sudo make install
+make install
 make clean
-sudo mv `which bochs` `which bochs`686dbg
+mv `which bochs` `which bochs`686dbg
 
 ./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --enable-usb --enable-pci --enable-cpu-level=6 --enable-memtype
 make -j 8 all
-sudo make install
+make install
 make clean
-sudo mv `which bochs` `which bochs`686
+mv `which bochs` `which bochs`686
 
 ./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --disable-usb --disable-pci --enable-cpu-level=4 --disable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
 make -j 8 all
-sudo make install
+make install
 make clean
-sudo mv `which bochs` `which bochs`486dbg
+mv `which bochs` `which bochs`486dbg
 
 ./configure --with-x11 --with-sdl2 --with-nogui --disable-docbook --disable-usb --disable-pci --enable-cpu-level=4 --disable-memtype
 make -j 8 all
-sudo make install
+make install
 make clean
-sudo mv `which bochs` `which bochs`486
+mv `which bochs` `which bochs`486
 
 cd ..
 rm -rf bochs
