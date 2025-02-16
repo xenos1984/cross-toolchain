4813c068d32315c13c05664372c445998d9c298f
xenos1984 <mhohmann@physnet.uni-hamburg.de>
2025-02-16 16:01:17 +0200

    Updated Bochs build to get code from Github.

--- test-bochs/build.sh
+++ test-bochs/build.sh
@@ -2,34 +2,34 @@
 set -e
 
 cd /usr/src
-svn co http://svn.code.sf.net/p/bochs/code/trunk/bochs bochs
-cd bochs
+git clone https://github.com/bochs-emu/Bochs.git
+cd Bochs/bochs
 
-./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-smp --enable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
+./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-evex --enable-amx --enable-smp --enable-memtype --enable-debugger --disable-debugger-gui
 make -j $(nproc --all) all
 make install
 make clean
 mv `which bochs` `which bochs`64mpdbg
 
-./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-smp --enable-memtype
+./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-evex --enable-amx --enable-smp --enable-memtype
 make -j $(nproc --all) all
 make install
 make clean
 mv `which bochs` `which bochs`64mp
 
-./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
+./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-evex --enable-amx --enable-memtype --enable-debugger --disable-debugger-gui
 make -j $(nproc --all) all
 make install
 make clean
 mv `which bochs` `which bochs`64dbg
 
-./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-memtype
+./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --enable-usb --enable-pci --enable-x86-64 --enable-avx --enable-evex --enable-amx --enable-memtype
 make -j $(nproc --all) all
 make install
 make clean
 mv `which bochs` `which bochs`64
 
-./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --enable-usb --enable-pci --enable-cpu-level=6 --enable-smp --enable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
+./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --enable-usb --enable-pci --enable-cpu-level=6 --enable-smp --enable-memtype --enable-debugger --disable-debugger-gui
 make -j $(nproc --all) all
 make install
 make clean
@@ -41,7 +41,7 @@ make install
 make clean
 mv `which bochs` `which bochs`686mp
 
-./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --enable-usb --enable-pci --enable-cpu-level=6 --enable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
+./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --enable-usb --enable-pci --enable-cpu-level=6 --enable-memtype --enable-debugger --disable-debugger-gui
 make -j $(nproc --all) all
 make install
 make clean
@@ -53,7 +53,7 @@ make install
 make clean
 mv `which bochs` `which bochs`686
 
-./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --disable-usb --disable-pci --enable-cpu-level=4 --disable-memtype --enable-debugger --enable-disasm --disable-debugger-gui
+./configure --with-x11 --with-sdl2 --with-term --with-nogui --disable-docbook --disable-usb --disable-pci --enable-cpu-level=4 --disable-memtype --enable-debugger --disable-debugger-gui
 make -j $(nproc --all) all
 make install
 make clean
@@ -65,5 +65,5 @@ make install
 make clean
 mv `which bochs` `which bochs`486
 
-cd ..
-rm -rf bochs
+cd ../..
+rm -rf Bochs
